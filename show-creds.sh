cb38bba4de9b13dc04fa8a24bfa297f6b721f442
rml1997 <rml1997@users.noreply.github.com>
2020-10-14 20:09:24 +0100

    Speed up by caching result and removing region key

--- show-creds.sh
+++ show-creds.sh
@@ -1,4 +1,8 @@
-export USER_OCID=$(oci iam user list --all | jq -r '.data |sort_by(."time-created")| .[0]."id"')
+# Get important login values
+USERLIST=$(oci iam user list --all)
+export USER_OCID=$(echo $USERLIST | jq -r '.data |sort_by(."time-created")| .[0]."id"')
+export TENANCY_OCID=$(echo $USERLIST | jq -r '.data[0]."compartment-id"')
+export REGION=$(oci iam region-subscription list | jq -r '.data[0]."region-name"')
 if [ ! -f ~/oci-keys/oci_api_key.pem ]; then
 # Generate private key
 mkdir ~/oci-keys
@@ -8,11 +12,7 @@ openssl rsa -pubout -in ~/oci-keys/oci_api_key.pem -out ~/oci-keys/oci_api_key_p
 # upload public key
 oci iam user api-key upload --user-id $USER_OCID --key-file ~/oci-keys/oci_api_key_public.pem
 fi
-# Get important login values
 export KEY_FINGERPRINT=$(oci iam user api-key list --user-id $USER_OCID | jq -r '.data |sort_by(."time-created")| .[-1]."fingerprint"')
-export TENANCY_OCID=$(oci iam user list --all | jq -r '.data[0]."compartment-id"')
-export REGION=$(oci iam region-subscription list | jq -r '.data[0]."region-name"')
-export REGION_KEY=$(oci iam region-subscription list | jq -r '.data[0]."region-key"')
 reset && echo -e "# Copy and paste the following into the Katacoda terminal:\n# Echo the login details into the correct file path in Katacoda\nmkdir ~/.oci\n
 echo \"[DEFAULT]\nuser=$USER_OCID\nfingerprint=$KEY_FINGERPRINT\ntenancy=$TENANCY_OCID\nregion=$REGION\nkey_file=~/.oci/oci_api_key.pem\" >  ~/.oci/config\n
 # Echo the private key into the correct file path in Katacoda. Keep this secret!\n
